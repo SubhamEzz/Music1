2290ccb04e5df9cc729fd9a63662477bd3e52e0d
SubhamEzz <1941012541.o.subhamkumar@gmail.com>
2021-07-21 13:22:09 +0530

    JqueryCode

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+
+
+/** with jQuery*/
+
+
+// $("button.w").on("click",function(){
+//   var audio = new Audio('sounds/tom-1.mp3');
+//              audio.play();
+//
+// });
+// $("button.a").on("click",function(){
+//   var audio = new Audio('sounds/tom-2.mp3');
+//           audio.play();
+// });
+// $("button.s").on("click",function(){
+//   var audio = new Audio('sounds/tom-3.mp3');
+//             audio.play();
+// });
+// $("button.d").on("click",function(){
+//   var audio = new Audio('sounds/tom-4.mp3');
+//                audio.play();
+// });
+// $("button.j").on("click",function(){
+//   var audio = new Audio('sounds/snare.mp3');
+//                audio.play();
+// });
+// $("button.k").on("click",function(){
+//   var audio = new Audio('sounds/crash.mp3');
+//                audio.play();
+// });$("button.l").on("click",function(){
+//   var audio = new Audio('sounds/kick-bass.mp3');
+//                audio.play();
+// });
+
+// $("document").keydown(function(event){
+//   playSound(event.key);
+//   btnAnimation(event.key);
+// });
+
+
+
+
+
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
 document.querySelectorAll("button")[i].addEventListener("click",function(){
   var btn = this.innerHTML;
@@ -5,11 +48,13 @@ document.querySelectorAll("button")[i].addEventListener("click",function(){
   btnAnimation(btn);
 
 });
+
 document.addEventListener("keydown",function(event){
   playSound(event.key);
   btnAnimation(event.key);
 });
 }
+
 function playSound(key){
   switch(key){
     case 'w' :   var audio = new Audio('sounds/tom-1.mp3');
